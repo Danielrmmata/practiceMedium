57671c8556b6ae0fd6b3d2524090ac919519141a
Daniel <danielrmmata@gmail.com>
2023-10-27 21:47:57 -0600

    proyectoInicial

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import React from "react"
+import "./App.scss"
+import Home from "../pages/home/home.js"
+
+export default function App() {
+
+    return(
+        <React.Fragment>
+            <Home />
+        </React.Fragment>
+    )
+}
